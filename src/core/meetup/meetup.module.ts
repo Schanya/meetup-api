c2951ac6b3819b9adf64e74ce6850d9fe1f0f5f6
Schanya <anyashemelevadm@gmail.com>
2023-02-27 17:05:11 +0300

    add meetup's entity

--- src/core/meetup/meetup.module.ts
+++ src/core/meetup/meetup.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
+import { SequelizeModule } from '@nestjs/sequelize';
+
+import { Meetup } from './domain/meetup.entity';
 import { MeetupController } from './application/meetup.controller';
 import { MeetupService } from './domain/meetup.service';
 
 @Module({
-	imports: [],
+	imports: [SequelizeModule.forFeature([Meetup])],
 	controllers: [MeetupController],
 	providers: [MeetupService],
 	exports: [MeetupService],
