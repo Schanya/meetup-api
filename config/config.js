4c4f76b78cc0ed0cb8d8e902357e240c7aebadd6
Schanya <anyashemelevadm@gmail.com>
2023-06-03 22:28:42 +0300

    edit config for migration

---
+++ config/config.js
@@ -0,0 +1,12 @@
+import { config } from 'dotenv';
+config();
+
+module.exports = {
+    "production": {
+        "username": process.env.POSTGRES_DB_USERNAME,
+        "password": process.env.POSTGRES_DB_PASSWORD,
+        "database": process.env.POSTGRES_DB_NAME,
+        "host": process.env.POSTGRES_DB_HOST,
+        "dialect": "postgres",
+    }
+}
